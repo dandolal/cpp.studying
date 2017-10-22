af9bd163801e2b45f948719b2d97d511ded861c4
dandolal <32968317+dandolal@users.noreply.github.com>
2017-10-22 22:02:34 +0300

    Update insertion-sort.cpp

--- sort/insertion-sort.cpp
+++ sort/insertion-sort.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
-#include <ctime>
-#include <iomanip>
-
-
-void insertionSort(int, int*);
+void insertionSort (int elemNumber, int* arr ) {
+    int j,c;
+    for (int i=1; i<elemNumber; ++i){
+        j=i-1;
+        c=arr[i];
+        while ((j>=0)&&(arr[j]>c)){
+            arr[j+1]=arr[j];
+            arr[j]=c;
+            j--;
+        }
+    }
+}
 
 int main(){
-int n,i;
-std::cin>>n;
-int *mas = new int [n];
-for (i=0;i<n;i++){
-    std::cin>>mas[i];
-}
-insertionSort(n, mas);
-for (i=0;i<n;i++){
-    std::cout<<mas[i]<<" ";
-}
-delete [] mas;
-return 0;
-}
-void insertionSort (int n, int* mas) {
-    int i,j,k,c;
-for (i=1;i<n;i++){
-    j=i-1;
-    while ((mas[j]>mas[i])&&(j>=0)){
-        j=j-1;
+    int elemNumber;
+    std::cin >> elemNumber;
+    int* arr = new int [elemNumber];
+    for (int i=0; i<elemNumber; ++i){
+        std::cin >> arr[i];
     }
-    c=mas[i];
-    for (k=i-1;k>j;k--){
-        mas[k+1]=mas[k];
+    insertionSort(elemNumber, arr);
+    for (int i=0; i<elemNumber; ++i){
+        std::cout << arr[i] << " ";
     }
-    mas[j+1]=c;
-}
+    delete[] arr;
+    return 0;
 }
