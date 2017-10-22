44eaa5c287c39391c192a00c5cb7396f5fdf7516
dandolal <32968317+dandolal@users.noreply.github.com>
2017-10-22 22:15:42 +0300

    Update insertion-sort.cpp

--- sort/insertion-sort.cpp
+++ sort/insertion-sort.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 void insertionSort (int elemNumber, int* arr ) {
     int j,c;
-    for (int i=1; i<elemNumber; ++i){
-        j=i-1;
-        c=arr[i];
-        while ((j>=0)&&(arr[j]>c)){
-            arr[j+1]=arr[j];
-            arr[j]=c;
-            j--;
+    for (int i = 1; i < elemNumber; ++i){
+        j = i-1;
+        c = arr[i];
+        while ((j >= 0)&&(arr[j] > c)){
+            arr[j+1] = arr[j];
+            arr[j] = c;
+            --j;
         }
     }
 }
@@ -16,11 +16,11 @@ int main(){
     int elemNumber;
     std::cin >> elemNumber;
     int* arr = new int [elemNumber];
-    for (int i=0; i<elemNumber; ++i){
+    for (int i = 0; i < elemNumber; ++i){
         std::cin >> arr[i];
     }
     insertionSort(elemNumber, arr);
-    for (int i=0; i<elemNumber; ++i){
+    for (int i = 0; i < elemNumber; ++i){
         std::cout << arr[i] << " ";
     }
     delete[] arr;
