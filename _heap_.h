63bdda969c481998e9c7149beec6897ce16c8a94
dandolal <32968317+dandolal@users.noreply.github.com>
2017-10-30 00:22:56 +0300

    Update _heap_.h

--- _heap_.h
+++ _heap_.h
@@ -2,26 +2,26 @@
 const int buffSize = 1000000;
 
 class Heap {
-    
+
     int* buff;
     int size;
-    
+
   public:
-      
+
     ~Heap() {
         delete[] buff;
     }
-    
+
     Heap(int elemnumber, int* arr) {
         size = elemnumber;
         int* buff = new int[buffSize];
-        build(arr, size) 
+        build(arr, size);
     }
-   
+
     int getSize() const {
         return size;
     }
-    
+
     void siftDown(int i) {
         while (2 * i + 1 <= size - 1) {
             int k = 2 * i + 1;
@@ -33,36 +33,36 @@ class Heap {
             }
         }
     }
-    
+
     void siftUp(int i) {
         while (buff[i] < buff[(i - 1) / 2]) {
             std::swap(buff[i], buff[(i - 1) / 2]);
             i = (i - 1) / 2;
         }
     }
-    
+
     void build(int* arr, int size) {
         for (int j = 0; j < size; ++j) {
             buff[j] = arr[j];
-            siftUp(j, buff);
+            siftUp(j);
         }
     }
-    
+
     int eraseMin() {
         int min = buff[0];
         buff[0] = buff[size - 1];
         --size;
-        sifDown(0);
+        siftDown(0);
         return min;
     }
-    
+
     void newElem(int newElem) {
         ++size;
         buff[size - 1];
         siftUp(size - 1);
     }
-    
+
     int get_min() {
         return buff[0];
     }
-}
+};
