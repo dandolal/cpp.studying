2110dc08305b26427e1a43f6f3f6a2538cb6650d
dandolal <32968317+dandolal@users.noreply.github.com>
2017-10-29 22:12:06 +0300

    Update _heap_.h

--- _heap_.h
+++ _heap_.h
@@ -1,26 +1,31 @@
 #include <iostream>
 const int buffSize = 1000000;
 
-
 class Heap {
-
+    
+    int* buff;
+    int size;
+    
   public:
-    ~Heap()
-    Heap(int elemNumber, int* arr) {
+      
+    ~Heap() {
+        delete[] buff;
+    }
+    
+    Heap(int elemnumber, int* arr) {
+        size = elemnumber;
         int* buff = new int[buffSize];
+        build(arr, size) 
     }
-    void copyArr(int* arr, int* buff, int elemnumber) {
-        for (int i = 0; i < elemnumber; ++i) {
-            buff[i] = arr[i];
-        }
-    }
+   
     int getSize() const {
-        return buffSize;
+        return size;
     }
-    void siftdown(int i, int* buff ,int right) {
-        while (2 * i + 1 <= right - 1) {
+    
+    void siftDown(int i) {
+        while (2 * i + 1 <= size - 1) {
             int k = 2 * i + 1;
-            if ((2 * i + 2 < right - 1) && (buff[2 * i + 2] < buff[2 * i + 1]))
+            if ((2 * i + 2 < size - 1) && (buff[2 * i + 2] < buff[2 * i + 1]))
                 k = 2 * i + 2;
             if (buff[i] > buff[k]) {
                 std::swap(buff[i], buff[k]);
@@ -28,18 +33,35 @@ class Heap {
             }
         }
     }
-    void siftup(int i, int* buff) {
+    
+    void siftUp(int i) {
         while (buff[i] < buff[(i - 1) / 2]) {
             std::swap(buff[i], buff[(i - 1) / 2]);
             i = (i - 1) / 2;
         }
     }
-    void build(int* arr, int* buff, int right) {
-        for (int j = 0; j < right; ++j) {
+    
+    void build(int* arr, int size) {
+        for (int j = 0; j < size; ++j) {
             buff[j] = arr[j];
-            siftup(j, buff);
+            siftUp(j, buff);
         }
     }
+    
+    int eraseMin() {
+        int min = buff[0];
+        buff[0] = buff[size - 1];
+        --size;
+        sifDown(0);
+        return min;
+    }
+    
+    void newElem(int newElem) {
+        ++size;
+        buff[size - 1];
+        siftUp(size - 1);
+    }
+    
     int get_min() {
         return buff[0];
     }
