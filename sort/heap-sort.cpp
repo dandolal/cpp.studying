fc3a324238c641fb5b2566f708c111e15e239b0d
dandolal <32968317+dandolal@users.noreply.github.com>
2017-10-30 19:01:55 +0300

    Update heap-sort.cpp

--- sort/heap-sort.cpp
+++ sort/heap-sort.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "_heap_.h"
 
+void heapSort(int* arr, std::size_t elemNumber) {
+    Heap h = Heap(elemNumber, arr);
+    for (std::size_t i = 0; i < elemNumber; ++i) {
+        arr[i] = h.erase();
+    }
+}
 
 int main () {
     std::size_t elemNumber;
@@ -8,12 +14,9 @@ int main () {
     int* arr = new int[elemNumber];
     for (std::size_t i = 0; i < elemNumber; ++i)
         std::cin >> arr[i];
-    Heap h = Heap(elemNumber, arr);
-    for (std::size_t i = 0; i < elemNumber; ++i) {
-        arr[i] = h.erase();
+    heapSort(arr, elemNumber);
+    for (std::size_t i = 0; i < elemNumber; ++i)
         std::cout << arr[i] << " ";
-    }
     delete[] arr;
-
     return 0;
-}
+} 
